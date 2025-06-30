6a063349b636b6bd17b36971d6839c9d13219341
josedevricardo <josericardoprogramadorgithub@gmail.com>
2025-06-30 11:23:12 -0300

    Atualização no .gitignore e firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhjSRSbttHjikA3gkm-jgC-jCBCvltR18",
-  authDomain: "cardapioplanetsburguer.firebaseapp.com",
-  databaseURL: "https://cardapioplanetsburguer-default-rtdb.firebaseio.com",
-  projectId: "cardapioplanetsburguer",
-  storageBucket: "cardapioplanetsburguer.appspot.com",
-  messagingSenderId: "797298710185",
-  appId: "1:797298710185:web:e0a2c92ef353ace8fa3bb8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
